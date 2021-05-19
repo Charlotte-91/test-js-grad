7b4644860c0a432eed467ed5d68ec69bc14f9824
Charlotte-91 <c.smallwood.smith@gmail.com>
2021-05-19 16:45:14 +0100

    Completed all 3 tasks

--- src/03-organise-maintainers/index.js
+++ src/03-organise-maintainers/index.js
@@ -38,9 +38,48 @@ The results should have this structure:
  * NOTE: the parent array and each "packageNames" array should 
  * be in alphabetical order.
  */
+const fetch = require('node-fetch');
 
 module.exports = async function organiseMaintainers() {
-  // TODO
+  const packageArray = []
+  const maintainers = []
+  const packageMaintainers = []
+  const usernames = []
+  const body = {
+    "url": "https://api.npms.io/v2/search/suggestions?q=react",
+    "method": "GET",
+    "return_payload": true}
+    const response = await fetch('http://ambush-api.inyourarea.co.uk/ambush/intercept',{
+      method: 'POST',
+      body: JSON.stringify(body)});
+      const data = await response.json();
 
-  return maintainers
+      (data.content).forEach(obj => {
+        packageArray.push(obj.package)
+      });
+
+      packageArray.forEach(obj =>
+        obj.maintainers.map((item) => usernames.push(item.username))
+      );
+      const uniq_usernames = [...new Set(usernames)];
+    
+      packageArray.forEach(obj =>
+        packageMaintainers.push({package: obj.name, maintainers: obj.maintainers.map((item) => item.username)})
+      );
+
+        (uniq_usernames).forEach(user => {
+          const userPackages = []
+          for(i = 0; i < packageMaintainers.length; i++){
+            if (((packageMaintainers[i].maintainers).includes(user)) == true) {
+              userPackages.push(packageMaintainers[i].package)
+            }
+          }
+          maintainers.push({
+            username: user,
+            packageNames: userPackages.sort()
+          })
+        })
+
+      maintainers.sort((a, b) => a.username.localeCompare(b.username));
+      return maintainers
 };
